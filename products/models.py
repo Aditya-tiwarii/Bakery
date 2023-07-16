1be42612818244cf60cabf1275d20982e9a79684
Aditya Nath Tiwari <85993693+Aditya-tiwarii@users.noreply.github.com>
2023-07-16 12:51:42 +0530

    Update models.py

--- products/models.py
+++ products/models.py
@@ -57,20 +57,3 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
     
-class Blog(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    blog_title = models.CharField(max_length=100)
-    blog_description = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="category")
-    post_date = models.DateTimeField(auto_now_add=True)
-    is_public = models.BooleanField(default=True)
-    slug = models.CharField(max_length=1000, null=True, blank=True)
-
-    def __str__(self):
-        return self.blog_title
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            base_slug = slugify(self.blog_title + " " + self.author.username + " " + self.category.category_name)
-            self.slug = base_slug + ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))
-        return super().save(*args, **kwargs)
